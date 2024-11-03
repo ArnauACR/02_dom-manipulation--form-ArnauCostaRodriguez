558bbb754652fa2050f1eca61fbd75f1f1f95aaf
ArnauACR <145784953+ArnauACR@users.noreply.github.com>
2024-11-03 12:36:15 +0100

    FIX: JS

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-const form = document.querySelector('form');
-const displaySection = document.querySelector('.collectNotes');
+    document.addEventListener('DOMContentLoaded', () => {
+        
+        const form = document.querySelector('form');
+        const displaySection = document.querySelector('.collectNotes');
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
+        form.addEventListener('submit', (event) => {
+        event.preventDefault();
     
-    const formData = new FormData(form);
-    const sanitizedData = {};
-    formData.forEach((value, key) => {
-    sanitizedData[key] = sanitizeInput(value);
+        const formData = new FormData(form);
+        const sanitizedData = {};
+
+        formData.forEach((value, key) => {
+            sanitizedData[key] = sanitizeInput(value);
+        });
+    
+        if (sanitizedData.priority === 'select') {
+            console.log('No priority');
+            return;
+        }
+
+        const newNode = document.createElement('div');
+        newNode.textContent = `Title: ${sanitizedData.title}, Content: ${sanitizedData.content}, Priority: ${sanitizedData.priority}`;
+    
+        displaySection.appendChild(newNode);
+    
+        form.reset();
+        });
+    
+        function sanitizeInput(input) {
+        const element = document.createElement('div');
+        element.innerText = input;
+        return element.innerHTML;
+        }
     });
-
-    if (sanitizedData.priority === 'select') {
-    console.log('No priority');
-    return;
-    }
-
-    const newNode = document.createElement('div');
-    newNode.textContent = `Title: ${sanitizedData.title}, Content: ${sanitizedData.content}, Priority: ${sanitizedData.priority}`;
     
-    displaySection.appendChild(newNode);
-
-    form.reset();
-});
-
-function sanitizeInput(input) {
-    const element = document.createElement('div');
-    element.innerText = input;
-    return element.innerHTML;
-}
-});
 
 
